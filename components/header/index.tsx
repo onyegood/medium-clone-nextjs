95c3a311c31b28333cdf7045912b5d775d0b3ee1
onyegood <goodmomen@gmail.com>
2022-08-12 03:21:32 +0100

    Started working on header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,6 +1,25 @@
+import Link from "next/link";
+
 const HeaderComponent = () => {
   return (
-    <div>Header Component here</div>
+    <header>
+      <div className="flex">
+        <Link href="/">
+          <img
+            className="w-44 object-contain cursor-pointer"
+            src="/assets/img/logo.jpeg"
+            alt="Medium Logo"
+          />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3>Follow</h3>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </header>
   )
 }
 
