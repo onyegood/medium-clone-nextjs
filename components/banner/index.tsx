db7a74ca259084529f0ab49bae6bcf202fa7c23d
onyegood <goodmomen@gmail.com>
2023-03-01 15:05:28 +0100

    testing

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const BannerComponent = () =>{
+const BannerComponent = () => {
   return (
-    <div
-      className="flex items-center justify-between bg-yellow-400 border-y border-black py-10 lg:py-0"
-    >
+    <div className="flex items-center justify-between bg-yellow-400 border-y border-black py-10 lg:py-0">
       <div className="px-10 space-y-5">
         <h1 className="text-6xl font-serif max-w-xl">
           <span className="underline decoration-black decoration-4">
             Medium
-          </span>
-          {' '}
-          is a place to read, write, and connect.
+          </span>{' '}
+          is a place to read, write, and connect
         </h1>
-        <h2>It's easy to post your thinking on any topic and connect with millions of reader.</h2>
+        <h2>
+          It's easy to post your thinking on any topic and connect with millions
+          of reader.
+        </h2>
       </div>
       <img
         className="hidden md:inline h-32 lg:h-full"
@@ -21,7 +21,7 @@ const BannerComponent = () =>{
         alt="Medium Logo"
       />
     </div>
-  )
-}
+  );
+};
 
 export default BannerComponent;
